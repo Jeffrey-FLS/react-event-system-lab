28cc86484fc38e110fb4ea316b3e58bb165eacea
Jeffrey Paulino <jp7kode@gmail.com>
2019-12-11 18:29:09 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, {Component} from 'react';
+
+class EyesOnMe extends Component {
+
+  focus = () => {
+    console.log("Good!")
+  };
+
+  render() {
+    return(
+      <button
+        onFocus={() => {console.log("Good!")}}
+        onBlur={() => {console.log("Hey! Eyes on me!")}}>
+        'Hey! Eyes on me!'
+      </button>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,30 @@
 // Code Keypad Component Here
+
+import React, { Component } from  'react';
+
+class Keypad extends Component {
+
+  // state = {
+  //   inputValue: ""
+  // };
+
+
+  changeInputValue = (event) => {
+    console.log('Entering password...');
+  };
+
+  render() {
+    return(
+      <input type="password" onKeyUp={this.changeInputValue}/>
+    );
+  }
+}
+
+export default Keypad;
+
+// onChange={this.changeInputValue}
+// value={this.state.inputValue}
+
+// this.setState({
+//   inputValue: event.target.value
+// });
